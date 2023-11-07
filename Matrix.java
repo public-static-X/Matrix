4d1e7210cb36eccb910e8ec9d225161c7dfd1081
public-static-X <87133416+public-static-X@users.noreply.github.com>
2023-11-07 22:48:38 +0200

    Add files via upload
    a Matrix class I made mainly for the purpose of solving for the inverse of a square real matrix. It uses a QR decomposition with the Gram-Schmidt process and then a basic inverse matrix formula to solve for the inverse

---
+++ Matrix.java
@@ -0,0 +1,311 @@
+public class Matrix {
+    
+    private int nrow;
+    private int ncol;
+    private double[][] matrix;
+    private double[][] Q;
+
+    public Matrix(double[][] matrix) {
+
+        this.nrow = matrix.length;
+        this.ncol = matrix[0].length;
+        this.matrix = new double[this.nrow][this.ncol];
+        
+        for (int i = 0; i < this.nrow; i++) {
+            for (int j = 0; j < this.ncol; j++) {
+                this.matrix[i][j] = matrix[i][j];
+            }
+        }
+    }
+
+    public int getNumRows(){
+        return this.nrow;
+    }
+
+    public int getNumCols(){
+        return this.ncol;
+    }
+
+    @Override
+    public String toString() {
+        String printer = "";
+        for (int i = 0; i < this.nrow; i++) {
+            printer += "|";
+            for (int j = 0; j < this.ncol; j++) {
+                printer += (this.matrix[i][j] + " ");
+            }
+        printer += "|\n";
+        }
+        return printer;
+    }
+
+    public Matrix add(Matrix B) {
+        double[][] result = new double[this.nrow][this.ncol];
+
+        for (int i = 0; i < this.nrow; i++) {
+            for (int j = 0; j < this.ncol; j++) {
+                result[i][j] = this.matrix[i][j] + B.matrix[i][j];
+            }
+        }
+
+        return new Matrix(result);
+    }
+
+    public Matrix subtract(Matrix B) {
+        double[][] result = new double[this.nrow][this.ncol];
+
+        for (int i = 0; i < this.nrow; i++) {
+            for (int j = 0; j < this.ncol; j++) {
+                result[i][j] = this.matrix[i][j] - B.matrix[i][j];
+            }
+        }
+
+        return new Matrix(result);
+    }
+
+    public Matrix getRow(int index) {
+        double[][] rowMatrix = new double[1][this.ncol];
+        for (int i = 0; i < this.ncol; i++) {
+            rowMatrix[0][i] = this.matrix[index][i];
+        }
+        return new Matrix(rowMatrix);
+    }
+
+    public Matrix getColumn(int index) {
+        double[][] colMatrix = new double[this.nrow][1];
+        for (int i = 0; i < this.nrow; i++) {
+            colMatrix[i][0] = this.matrix[i][index];
+        }
+        return new Matrix(colMatrix);
+    }
+
+    public Matrix transpose() {
+        double[][] temp = new double[this.ncol][this.nrow];
+
+        for (int i = 0; i < this.ncol; i++) {
+            for (int j = 0; j < this.nrow; j++) {
+                temp[i][j] = this.matrix[j][i];
+            }
+        }
+
+        return new Matrix(temp);
+    }
+        
+    public static Matrix Identity(int N) {
+
+        double[][] ident = new double[N][N];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (i == j) {
+                    ident[i][j] = 1;
+                } else {
+                    ident[i][j] = 0;
+                }
+            }
+        }
+        return new Matrix(ident);
+    }
+
+    public double getElement(int row, int col) {
+        return this.matrix[row][col];
+    }
+
+    private double vectorMult(Matrix B) {
+        double result = 0;
+        for (int i = 0; i < this.ncol; i++) {
+            result += this.getElement(0, i) * B.getElement(i, 0);
+        }
+        return result;
+    }
+
+    public double dotProduct(Matrix vector) { // 2 vectors of same array size multiplied.
+        double result = 0;
+
+        if(this.isColumnVector() && vector.isColumnVector()) {
+            result = this.transpose().vectorMult(vector);
+        } else if(this.isRowVector() && vector.isRowVector()) {
+            result = this.vectorMult(vector.transpose());
+        } else if(this.isRowVector() && vector.isColumnVector()) {
+            result = this.vectorMult(vector);
+        } else if(this.isColumnVector() && vector.isRowVector()) {
+            result = this.transpose().vectorMult(vector.transpose());
+        } else {
+            throw new IllegalArgumentException("Vectors not conformal for multiplication");
+        }
+
+    return result;
+    }
+
+    public Matrix multiplyMatrix(Matrix MatrixTwo) {
+
+        double[][] result = new double[this.nrow][MatrixTwo.ncol];
+
+        for (int i = 0; i < this.nrow; i++) {
+            for (int j = 0; j < MatrixTwo.ncol; j++) {
+                result[i][j] = this.getRow(i).dotProduct(MatrixTwo.getColumn(j));
+            }
+        }
+        return new Matrix(result);
+    }
+
+    public Matrix scalarMultiply(double scalar) {
+        double[][] result = new double[this.nrow][this.ncol];
+        for (int i = 0; i < this.nrow; i++) {
+            for (int j = 0; j < this.ncol; j++) {
+                result[i][j] = this.matrix[i][j] * scalar;
+            }
+        }
+        return new Matrix(result);
+    }
+
+    private boolean isRowVector() { // vector needs to be like [1,2,1] for example. must be a row vector
+        return this.nrow == 1;
+    }
+
+    private boolean isColumnVector() { // vector needs to be like [1,2,1].transpose() or in column format
+        return this.ncol == 1;
+    }
+
+    
+
+    public double norm() { // maginitude of vector x = ||x||
+        double result = 0;
+        if(this.isRowVector() || this.isColumnVector()){
+        result = this.dotProduct(this);
+        } else{
+            throw new IllegalArgumentException("Not a vector");
+        }
+        return Math.sqrt(result);
+    }
+
+
+    public Matrix normalizeVector() { // Mathematically saying x / ||x|| which gives a magnitude of 1
+        //double[][] vector = new double[this.nrow][this.ncol];
+        if(this.isRowVector() || this.isColumnVector()) {
+            return this.scalarMultiply(1 / this.norm()) ;
+        } else {
+            throw new IllegalArgumentException("Not a vector");
+        }
+    }
+
+    public static Matrix projection(Matrix u, Matrix a) { // proj_u_(a) = <u,a>/<u,u> .u = scalar multiple of u
+        Matrix proj =u.scalarMultiply( (u.dotProduct(a) / u.dotProduct(u)) );
+        return proj;
+    }
+
+    public Matrix columnBind(Matrix C) {
+        
+        if(this.nrow != C.nrow) throw new IllegalArgumentException("Incompatible number of rows");
+
+        int numCol = this.ncol + C.ncol;
+        double[][] newMat = new double[this.nrow][numCol];
+        
+        for (int i = 0; i < this.nrow; i++) {
+            for (int j = 0; j < this.ncol; j++) {
+                newMat[i][j] = this.matrix[i][j];
+            }
+        }
+
+        for (int i = 0; i < this.nrow; i++) {
+            for (int j = this.ncol; j < numCol; j++) {
+                newMat[i][j] = C.matrix[i][j - this.ncol];
+            }
+        }
+
+        return new Matrix (newMat);
+    }
+
+    public Matrix rowBind(Matrix C) {
+        
+        if(this.ncol != C.ncol) throw new IllegalArgumentException("Incompatible number of columns");
+
+        int numRow = this.nrow + C.nrow;
+        double[][] newMat = new double[numRow][this.ncol];
+        
+        for (int i = 0; i < this.nrow; i++) {
+            for (int j = 0; j < this.ncol; j++) {
+                newMat[i][j] = this.matrix[i][j];
+            }
+        }
+
+        for (int i = this.nrow; i < numRow; i++) {
+            for (int j = 0; j < this.ncol; j++) {
+                newMat[i][j] = C.matrix[i - this.nrow][j];
+            }
+        }
+
+        return new Matrix (newMat);
+    }
+    
+    public void QRdecomposition() {
+
+        if(this.ncol != this.nrow) throw new IllegalArgumentException("Currently only works for square matrices");
+
+        Matrix U = this.getColumn(0).normalizeVector();
+        Matrix column;
+        
+        for (int i = 1; i < this.ncol; i++) {
+
+            column = this.getColumn(i);
+            
+            for (int j = 0; j < U.getNumCols(); j++) {
+                column = column.subtract( Matrix.projection(U.getColumn(j), column) );
+            }
+            U = U.columnBind(column.normalizeVector());
+        }
+        this.Q = U.matrix;
+    }
+
+    public Matrix get_Q_matrix() {
+        return new Matrix(this.Q);
+    }
+
+    public Matrix get_R_matrix() {
+        return this.get_Q_matrix().transpose().multiplyMatrix(this);
+    }
+
+    public Matrix Round() {
+        double[][] roundedMatrix = new double[this.nrow][this.ncol];
+        for (int i = 0; i < this.nrow; i++) {
+            for (int j = 0; j < this.ncol; j++) {
+               roundedMatrix[i][j] = Math.round(100 * this.matrix[i][j] ) / 100;
+            }
+        }
+        return new Matrix(roundedMatrix);
+    }
+
+    public Matrix back_substitution() {
+        double[][] X = new double[this.nrow][this.ncol];
+        for (int i = 0; i < this.nrow; i++) {
+            for (int j = 0; j < this.ncol; j++) {
+                X[i][j] = 0;
+            }
+        }
+
+        //int counter;
+
+        for (int column = 0; column < this.nrow; column++) {
+            double x;
+            int counter;
+
+            Matrix I = Matrix.Identity(this.nrow).getColumn(column);
+            for (int j = this.nrow - 1; j >= 0; j--) {
+                x = I.getElement(j, 0);
+                counter = this.nrow - 1;
+                while(counter > 0) {
+                    x = x - this.matrix[j][counter] * X[counter][column];
+                    counter--;
+                }
+                X[j][column] = x / this.matrix[j][j];
+            }
+        }
+            return new Matrix(X);
+    }
+
+    public Matrix inverse() {
+        this.QRdecomposition();
+        return this.get_R_matrix().back_substitution().multiplyMatrix(get_Q_matrix().transpose());
+    }
+    
+}
